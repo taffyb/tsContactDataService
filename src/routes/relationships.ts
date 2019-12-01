b0e077ff608af9b985699c9fc5eae64e9510f0bf
Taffy Brecknock <brecknockt@gmail.com>
2019-12-01 14:52:28 +0000

    get Relationship types

--- src/routes/relationships.ts
+++ src/routes/relationships.ts
@@ -14,11 +14,18 @@ export const register = ( app: express.Application, prefix: string= '/api' ) => 
     app.delete( prefix + '/relationships/:uuid', async ( req: any, res ) => {
        // remove a relationship by uuid
     });
-    app.get( prefix + '/relationships', async ( req: any, res ) => {
-
-        neo4jSvc.executeCypher('getRelationships.cyp', {})
-        .then(result => {console.log(`${JSON.stringify(result)}`); res.send(result[0].links); })
-        .catch(err => {console.log(`GET ${prefix}/relationships ${JSON.stringify(err)}`); res.sendStatus(500); });
+    app.get( prefix + '/relationships', ( req: any, res ) => {
+        console.log(`${JSON.stringify(req.params)}`);
+        const types: string[] = req.query.types;
+        if (!types) {
+            neo4jSvc.executeCypher('getRelationships.cyp', {})
+            .then(result => { res.send(result[0].links); })
+            .catch(err => {console.log(`GET ${prefix}/relationships ${JSON.stringify(err)}`); res.sendStatus(500); });
+        } else {
+            neo4jSvc.executeCypher('getRelationshipLabels.cyp', {sourceType: types[0], targetType: types[1]})
+            .then(result => { res.send(result[0].types); })
+            .catch(err => {console.log(`GET ${prefix}/relationships ${JSON.stringify(err)}`); res.sendStatus(500); });
+        }
     });
 };
 
